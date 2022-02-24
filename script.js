e72924715294e6d30226141a979a1ed03cad78e5
jairaaman006 <99857978+jairaaman006@users.noreply.github.com>
2022-02-24 15:19:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+1. Difference between Http 1 and Http 2?
+
+Http 1
+
+* It uses basic authentication scheme which is unsafe .
+* The error prompt is not specific enough .
+* For every tcp connection there is only one request and one response .
+
+Http 2
+
+* To reduce taffic in the page, http 2 uses multiplexing and server push .
+* upagrade in security measures .
+* Underlying semantics of http such as header,status code remain the same.
+
+2. Objects and its internal representation in JS
+
+* unassigned properties of an object are undefined and not null.
+* objects in JS are unorderd collection of related data . 
+* -Objects are more complex and each object may contain any combination of these primitive data-types as well as reference data-types.
+
+
+
